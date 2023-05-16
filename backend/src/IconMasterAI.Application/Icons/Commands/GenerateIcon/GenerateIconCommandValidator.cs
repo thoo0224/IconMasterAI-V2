eaf7444b5c0a8bc00c25d9caa404a2193442551e
Thomas <40025225+thoo0224@users.noreply.github.com>
2023-05-16 12:05:28 +0200

    add command validator

---
+++ backend/src/IconMasterAI.Application/Icons/Commands/GenerateIcon/GenerateIconCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace IconMasterAI.Application.Icons.Commands.GenerateIcon;
+
+internal sealed class GenerateIconCommandValidator 
+    : AbstractValidator<GenerateIconCommand>
+{
+    public GenerateIconCommandValidator()
+    {
+        RuleFor(x => x.Prompt)
+            .MaximumLength(1000)
+            .WithMessage("Prompt can't be longer than 1000 characters.");
+
+        // TODO: Rule for color
+        // TODO: Rule for style
+    }
+}
